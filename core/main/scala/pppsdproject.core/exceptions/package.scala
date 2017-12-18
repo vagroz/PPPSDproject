4761e79199bb44253994e6cf74569917acc27e7a
Подольцева Ольга Константиновна <Olyashka2010@yandex.ru>
2017-12-18 06:35:46 +0300

    add tables and fix primary keys to Option

--- core/main/scala/pppsdproject.core/exceptions/package.scala
+++ core/main/scala/pppsdproject.core/exceptions/package.scala
@@ -3,4 +3,6 @@ package pppsdproject.core
 package object exceptions {
   case class ListNotFoundException(message: String, cause: Throwable) extends Exception
 
+  case class NotFoundInDatabase(message: String, cause: Throwable) extends Exception
+
 }
--- dbservice/main/scala/pppsdproject.dbservice/tables/package.scala
+++ dbservice/main/scala/pppsdproject.dbservice/tables/package.scala
@@ -1,3 +1,40 @@
-package object tables {
+package pppsdproject.dbservice
 
+import pppsdproject.core.model._
+import slick.jdbc.PostgresProfile.api._
+
+package object tables {
+  case class BoardTable(tag: Tag) extends TableBoardDB](tag, "BOARD") {
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("name")
+
+    def * = (id, name, listId, description).mapTo[TaskDB]
+  }
+
+  lazy val boards = TableQuery[BoardTable]
+
+  case class ListTable(tag: Tag) extends Table[ListDB](tag, "LIST") {
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("name")
+    def boardId = column[Int]("boardId")
+
+    def * = (id, name, listId, description).mapTo[TaskDB]
+
+    def board = foreignKey("board_fK", boardId, boards)(._id, onDelete=ForeignKeyAction.NoAction, onUpdate=ForeignKeyAction.Cascade)
+  }
+
+  lazy val lists = TableQuery[ListTable]
+
+  case class TaskTable(tag: Tag) extends Table[TaskDB](tag, "TASK") {
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("name")
+    def listId = column[Int]("listId")
+    def description = column[Option[String]]("description")
+
+    def * = (id, name, listId, description).mapTo[TaskDB]
+
+    def list = foreignKey("list_fK", listId, lists)(._id, onDelete=ForeignKeyAction.Restrict, onUpdate=ForeignKeyAction.Cascade)
+  }
+
+  lazy val tasks = TableQuery[TaskTable]
 }
