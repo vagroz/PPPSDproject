7313cae2004b60c36d924af12123357d8ddc1eb1
Ivan Malyshev <ivan.malyshev@phystech.edu>
2017-12-19 16:55:35 +0300

    complete test

--- webserver/src/test/scala/pppsdproject/webserver/WebServerTest.scala
+++ webserver/src/test/scala/pppsdproject/webserver/WebServerTest.scala
@@ -5,12 +5,14 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.server._
 import Directives._
+import pppsdproject.core.model.TaskDB
 
 
 class WebServerTest
   extends FlatSpec
     with Matchers
-    with ScalatestRouteTest{
+    with ScalatestRouteTest
+    with WebProtocol{
 
   val route = WebServer.getRoute()
 
@@ -26,6 +28,30 @@ class WebServerTest
 
     Delete("/task/56") ~> route ~> check {
       status shouldBe StatusCodes.NoContent
+    }
+  }
+
+  it should "return task id's in list" in {
+    Get("/task?list=list1&board=board1") ~> route ~> check {
+      entityAs[WebResponse[Seq[Int]]].payload.get should contain only(1117, 1118, 1119)
+    }
+  }
+
+  it should "add task and return its id" in {
+    val request = AddTaskRequest(TaskDescription("Task1", Some("Very important task")), "list1", "board1")
+    Post("/task", request) ~> route ~> check {
+      entityAs[WebResponse[Int]].payload.get shouldBe 1117
+      status shouldBe StatusCodes.Created
+    }
+  }
+
+  it should "return task by Id" in {
+    val id = 56789
+    Get(s"/task/$id") ~> route ~> check {
+      entityAs[WebResponse[TaskDB]].payload.get.id shouldBe Some(id)
+    }
+    Get("/task/0") ~> Route.seal(route) ~> check {
+      status shouldBe StatusCodes.NotFound
     }
   }
 
