a5de21ef41844b50fa0e1e0f9fa6fc978c6c6602
Ivan Malyshev <ivan.malyshev@phystech.edu>
2017-12-19 16:54:49 +0300

    fix exception handling

--- webserver/src/main/scala/pppsdproject.webserver/WebServer.scala
+++ webserver/src/main/scala/pppsdproject.webserver/WebServer.scala
@@ -31,16 +31,6 @@ with WebProtocol {
 
   def runWithService (service: WebService) {
     srv = service
-
-    implicit def myExceptionHandler: ExceptionHandler = ExceptionHandler {
-      case th @ (TaskNotFountException(_,_) | ListNotFoundException(_,_) | BoardNotFoundException(_,_) )=>
-        complete (404, WebResponse[Int](WebStatus.Error, Some(th.getMessage), None))
-      case th: InternalError =>
-        complete (500, WebResponse[Int](WebStatus.Error, Some(th.getMessage), None))
-      case th: Throwable =>
-        complete (520, WebResponse[Int](WebStatus.Error, Some(th.getMessage), None))
-    }
-
     val webconf = Config.loadWebserviceConfig()
     val bindingFuture = Http().bindAndHandle(route, webconf.endpoint, webconf.port)
 
@@ -51,72 +41,83 @@ with WebProtocol {
       .onComplete(_ => system.terminate()) // and shutdown when done
   }
 
+  def myExceptionHandler: ExceptionHandler = ExceptionHandler {
+    case th @ (TaskNotFountException(_,_) | ListNotFoundException(_,_) | BoardNotFoundException(_,_) )=>
+      complete (404, WebResponse[Int](WebStatus.Error, Some(th.getMessage), None))
+    case th: InternalError =>
+      complete (500, WebResponse[Int](WebStatus.Error, Some(th.getMessage), None))
+    case th: Throwable =>
+      complete (520, WebResponse[Int](WebStatus.Error, Some(th.getMessage), None))
+  }
+
   val route =
-    path("hello") {
-      get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Hello, XXI Century World!!!</h1>"))
-      }
-    } ~
-      path("task" / IntNumber) { id =>
+    handleExceptions(myExceptionHandler){
+      path("hello") {
         get {
-          val futuResult = Future(srv.getTaskById(id))
-            .map (task => WebResponse(WebStatus.Ok, None, Some(task)))
-          onComplete(futuResult){ result =>
-            complete(result)
-          }
-        } ~
-          delete {
-            val futuResult = Future(srv.deleteTask(id))
-            onSuccess(futuResult){
-              complete(HttpResponse(204))
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Hello, XXI Century World!!!</h1>"))
+        }
+      } ~
+        path("task" / IntNumber) { id =>
+          get {
+            val futuResult = Future(srv.getTaskById(id))
+              .map (task => WebResponse(WebStatus.Ok, None, Some(task)))
+            onComplete(futuResult){ result =>
+              complete(result)
             }
           } ~
-          put {
-            parameters('list.as[String]) { listName =>
-              val moveReq = MoveTaskRequest(id, listName)
-              val futureResult = Future(srv.moveTask(moveReq))
-              onSuccess(futureResult){
+            delete {
+              val futuResult = Future(srv.deleteTask(id))
+              onSuccess(futuResult){
                 complete(HttpResponse(204))
               }
+            } ~
+            put {
+              parameters('list.as[String]) { listName =>
+                val moveReq = MoveTaskRequest(id, listName)
+                val futureResult = Future(srv.moveTask(moveReq))
+                onSuccess(futureResult){
+                  complete(HttpResponse(204))
+                }
 
+              }
             }
-          }
-      } ~
-      path("task") {
-        get {
-          parameters('list.as[String], 'board.as[String]) {(listName, boardName) =>
-            val futureResult = Future(srv.getTasksOnListInBoard(listName, boardName))
-              .map { result =>
-                WebResponse(WebStatus.Ok, None, Some(result))
+        } ~
+        path("task") {
+          get {
+            parameters('list.as[String], 'board.as[String]) {(listName, boardName) =>
+              val futureResult = Future(srv.getTasksOnListInBoard(listName, boardName))
+                .map { result =>
+                  WebResponse(WebStatus.Ok, None, Some(result))
+                }
+              onComplete(futureResult){ result =>
+                complete(result)
+              }
+            }
+          } ~
+            post {
+              entity(as[AddTaskRequest]) { inputData =>
+                val futureResult = Future(srv.addTask(inputData))
+                  .map { res =>
+                    WebResponse(WebStatus.Ok, None, Some(res))
+                  }
+                onComplete(futureResult){ result =>
+                  complete(201, result)
+                }
+              }
+            }
+        } ~
+        path ("list") {
+          parameters('board.as[String]){ board =>
+            val futureResult = Future(srv.getListsOnBoard(board))
+              .map { res =>
+                WebResponse(WebStatus.Ok, None, Some(res))
               }
             onComplete(futureResult){ result =>
               complete(result)
             }
           }
-        } ~
-          post {
-            entity(as[AddTaskRequest]) { inputData =>
-              val futureResult = Future(srv.addTask(inputData))
-                .map { res =>
-                  WebResponse(WebStatus.Ok, None, Some(res))
-                }
-              onComplete(futureResult){ result =>
-                complete(201, result)
-              }
-            }
-          }
-      } ~
-      path ("list") {
-        parameters('board.as[String]){ board =>
-          val futureResult = Future(srv.getListsOnBoard(board))
-            .map { res =>
-              WebResponse(WebStatus.Ok, None, Some(res))
-            }
-          onComplete(futureResult){ result =>
-            complete(result)
-          }
         }
-      }
+    }
 
   def getRoute() = {
     route
