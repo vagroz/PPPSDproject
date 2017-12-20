7a218b7818529060aac26848af5eec5e098b48d7
Ivan Malyshev <ivan.malyshev@phystech.edu>
2017-12-20 19:44:46 +0300

    in Main: run webserver on the realDB

--- src/main/scala/pppsdproject/Main.scala
+++ src/main/scala/pppsdproject/Main.scala
@@ -1,5 +1,17 @@
 package pppsdproject
 
-class Main {
+import pppsdproject.webserver._
+import pppsdproject.dbservice._
 
+object Main extends App {
+  if (args.length == 0) {
+    val dbService = new DataBaseServiceImpl
+    dbService.clearTables()
+    val webService = new WebServiceImpl(dbService)
+    WebServer.runWithService(webService)
+  }else if (args.toSet.contains("-t")){
+    println("Test mode: ON")
+    val webService = new WebServiceImpl(new FakeDb)
+    WebServer.runWithService(webService)
+  }
 }
